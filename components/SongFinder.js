5d1b84f769347c4ba49025fa48fe5953bbc39625
Niels Huisman <nielsz@pm.me>
2024-08-30 23:51:05 +0200

    va

--- components/SongFinder.js
+++ components/SongFinder.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 import useAsyncEffect from 'use-async-effect';
 import { getPlayListTracks, getPlaylists, getSavedTracks, getTracksAudioFeatures, getTracksByTempo } from '../app/spotify';
-import { Button, Card, CardBody, CardHeader, Chip, Input, Progress, Select, SelectItem, Switch } from '@nextui-org/react';
+import { Button, ButtonGroup, Card, CardBody, CardHeader, Chip, Input, Progress, Select, SelectItem, Switch } from '@nextui-org/react';
 import Image from 'next/image';
 import { IoIosCheckmark, IoIosCheckmarkCircle } from "react-icons/io";
 import { color } from 'framer-motion';
@@ -13,15 +13,13 @@ const LOADING_FEATURES = 2;
 const DONE = 3;
 
 export function SearchOptions({options, setOptions}) {
-  return <div>
-    <Select value={options.sorting_method} onChange={e => setOptions(options => (
-      {...options, sorting_method: e.target.value}))}>
-      <SelectItem value="slowest">Slowest</SelectItem>
-      <SelectItem value="fastest">Fastest</SelectItem>
-      <SelectItem value="closest">Closest</SelectItem>
+  return <div className='w-full flex gap-4'>  
+    <Select className='w-1/4' label='Sorting method' selectedKeys={[options.sorting_method]} onSelectionChange={sorting_method => setOptions(options => ( {...options, sorting_method}))}>
+      <SelectItem key="slowest">Slowest</SelectItem>
+      <SelectItem key="fastest">Fastest</SelectItem>
+      <SelectItem key="closest">Closest</SelectItem>
     </Select>
-    <Input type='number' value={options.tolerance} onChange={e => setOptions(options => (
-      {...options, tolerance: Number(e.target.value)}))} />
+    <Input className={'w-1/4'} label='Tolerance' type='number' value={options.tolerance} onValueChange={tolerance => setOptions(options => ({...options, tolerance}))} />
     <Switch defaultChecked={options.enable_half_and_double_time} onChange={e => setOptions(options => (
       {...options, enable_half_and_double_time: !options.enable_half_and_double_time}))}>
         Enable half and double time search
@@ -78,7 +76,7 @@ export function PlaylistLoader({setTracks}) {
 
   const pending = {isDisabled: true}
   const current = {color: 'primary'}
-  const done = {color: 'success', startContent: <IoIosCheckmarkCircle />}
+  const done = {color: 'success', variant: 'faded', startContent: <IoIosCheckmarkCircle />}
 
   return <>
     <PlaylistSelector onChange={e => setPlaylistId(e.target.value)}></PlaylistSelector>
@@ -121,9 +119,11 @@ export function PlaylistSelector({ onChange }) {
 export function TempoSelector({value, setValue}) {
   // const [value, setValue] = useState(100);
   return <>
-    <Input type="number" value={value} onChange={e=>setValue(Number(e.target.value))}></Input>
-    <Button onClick={() => setValue(prev => prev-1)}>-</Button>
-    <Button onClick={() => setValue(prev => prev+1)}>+</Button>
-    <h3>{value} BPM</h3>
+    {/* <Input type="number" value={value} onChange={e=>setValue(Number(e.target.value))}></Input> */}
+    <h2 className="text-2xl font-bold text-center">{value} BPM</h2>
+    <ButtonGroup>
+      <Button onClick={() => setValue(prev => prev-1)}>-</Button>
+      <Button onClick={() => setValue(prev => prev+1)}>+</Button>
+    </ButtonGroup>
   </>
 }
